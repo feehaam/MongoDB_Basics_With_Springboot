4595f26b0c5181f8e58878b58e073a61c17944d6
feehaam <shartaz.yeasar@bjitacademy.com>
2024-01-07 20:41:02 +0600

    First model ineup set

---
+++ src/main/java/com/feeham/playground/models/Course.java
@@ -0,0 +1,18 @@
+//package com.feeham.playground.models;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//import org.springframework.data.mongodb.core.mapping.Document;
+//
+//import java.util.List;
+//
+//@Getter @Setter @AllArgsConstructor @NoArgsConstructor
+//@Document(collation = "courses")
+//public class Course {
+//    private Long id;
+//    private String title;
+//    private String tutorName;
+//    private List<Student> enrolledStudents;
+//}
