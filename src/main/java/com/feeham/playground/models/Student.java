c0fe651ec98f37c390206e7fb9a8386912a83327
feehaam <shartaz.yeasar@bjitacademy.com>
2024-01-07 21:00:08 +0600

    Course & Enrollment system added

--- src/main/java/com/feeham/playground/models/Student.java
+++ src/main/java/com/feeham/playground/models/Student.java
@@ -4,11 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter @Setter @AllArgsConstructor @NoArgsConstructor
 @Document(collection = "students")
 public class Student {
+    @Id
     private Long id;
     private String name;
     private String roll;
